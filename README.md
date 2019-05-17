cb0023ff2c0afe1397f95e2558584d9603dadd5d
batTrung <trungbat.pv@gmail.com>
2019-05-17 15:12:47 +0700

    edit

---
+++ README.md
@@ -0,0 +1,45 @@
+#  Tự động hiện thị kết quả tìm kếm bằng AJAX
+
+<a target="_blank" href=""><img src="" alt="" /></a>
+
+
+## BLOG djangobat
+
+[https://www.djangobat.com/](https://www.djangobat.com/)
+
+## Cài đặt
+
+Đầu tiên, tải repository về máy tính:
+
+```bash
+$ git clone https://github.com/batTrung/jquery-autocomplete.git
+```
+
+Cài đặt requirements:
+
+```bash
+$ cd jquery-autocomplete
+$ source env/bin/activate
+```
+
+Tạo database:
+
+```bash
+$ python manage.py migrate
+```
+
+Hãy tạo một số người dùng để test ví dụ này 
+
+
+Chạy development server
+
+```bash
+$ python manage.py runserver
+```
+
+Mở Chrome hay FireFox : **127.0.0.1:8000**
+
+
+
+
+
---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from django.contrib.auth import get_user_model
+
+User =get_user_model()
+
+def search(request):
+	return render(request, "search.html")
+
+def get_search(request):
+	data= {}
+	input = request.GET.get("input")
+	if input == "":
+		data['is_exists'] = False
+		return JsonResponse(data)
+
+	users = User.objects.filter(username__contains=input)
+	if users.exists():
+		data['is_exists'] = True
+		data['html_value'] = render_to_string("includes/users.html",{'users':users})
+
+	else:
+		data['is_exists'] = False
+
+	return JsonResponse(data)
